1d4240359a296286111df0cb434b324e29ed85e0
Raushan <raushanvadshah@gmail.com>
2023-06-23 17:45:55 +0530

    Fixes Sort

---
+++ src/components/AltHeader.js
@@ -0,0 +1,35 @@
+import Logo from "../assets/Logo.png";
+import { useNavigate } from "react-router-dom";
+
+function Header(props) {
+  const navigate = useNavigate();
+  function handleClick() {
+    navigate("/");
+  }
+
+  return (
+    <div>
+      <div className="my-header">
+        <img
+          onClick={handleClick}
+          style={{ height: "43px", margin: "10px" }}
+          className=""
+          src={Logo}
+          alt="App Logo"
+        />
+        <button onClick={props.onSubmit} className="mybutton">
+          Generate New Array
+        </button>
+        <a href="https://github.com/">
+          <img
+            style={{ height: "43px", margin: "10px" }}
+            src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"
+            alt="git-logo"
+          ></img>
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
